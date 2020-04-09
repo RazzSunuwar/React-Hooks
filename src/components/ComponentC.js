26b8fc6f9e0f296ea8b59e5f1788c2900a1bd8a2
raysunwar <ray@whitehatagency.com.au>
2020-04-09 14:12:38 +0545

    React Hooks - useReducer with useContext

---
+++ src/components/ComponentC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentE from './ConponentE';
+
+function ComponentC() {
+    return (
+        <div>
+            <ComponentE />
+        </div>
+    )
+}
+
+export default ComponentC;
