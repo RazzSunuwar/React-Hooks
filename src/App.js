effc34990609e087e3a3b2bb4d4a8a5c3fb5d0b1
raysunwar <ray@whitehatagency.com.au>
2020-04-09 18:33:38 +0545

    React Hooks - Fetching data with useReducer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { useReducer } from 'react';
 import './App.css';
 
-// import ComponentA from './components/ComponentA';
-// import ComponentB from './components/ComponentB';
-// import ComponentC from './components/ComponentC';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
 import DataFetchingOne from './components/DataFetchingOne';
 
 
-// const initialState = 0;
-// const reducer = (state, action) => {
-//   switch(action) {
-//     case 'increment':
-//       return state + 1;
-//     case 'decrement':
-//       return state - 1;
-//     case 'Reset':
-//       return initialState
-//     default:
-//       return state
-//   }
-// }
+const initialState = 0;
+const reducer = (state, action) => {
+  switch(action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'Reset':
+      return initialState
+    default:
+      return state
+  }
+}
 
-// export const CountContext = React.createContext()
+export const CountContext = React.createContext()
 
 function App() {
-  // const [count, dispatch] = useReducer(reducer, initialState)
+  const [count, dispatch] = useReducer(reducer, initialState)
 
   return (
-    // <CountContext.Provider 
-    // value={{ countState: count, countDispatch: dispatch}}>
+    <CountContext.Provider 
+    value={{ countState: count, countDispatch: dispatch}}>
 
     <div className="App">
       {/* Coutn - {count} */}
@@ -38,7 +38,7 @@ function App() {
         <DataFetchingOne />
 
     </div>
-    // </CountContext.Provider>
+     </CountContext.Provider>
 
   );
 }
